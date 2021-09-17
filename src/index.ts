410fb1ea0a0a1b9730cd278b190857d9dfe455b1
Óscar <oscartrujillo94@gmail.com>
2021-09-17 18:07:35 +0200

    add apollo

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
-import { Request, Response } from "express";
+import { ApolloServer, ApolloServerExpressConfig } from 'apollo-server-express';
+import { Request, Response } from 'express';
 import express = require('express');
-import { graphqlHTTP } from "express-graphql";
-import schema from './graphql/graphql.index'
-
-//express initialization
-const app = express();
+import { graphqlHTTP } from 'express-graphql';
+import schema from './graphql/graphql.index';
 
 //PORT
 const PORT = 4000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+// configure the server here
+const serverConfig: ApolloServerExpressConfig = {
+  schema,
+  playground: {
+    settings: {
+      'editor.theme': 'dark',
+      'editor.cursorShape': 'line',
+    },
+  },
+};
+
+//express initialization
+const server = new ApolloServer(serverConfig);
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('go to http://localhost:4000//graphql');
 });
 
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
-
-//localhost setup
-app.listen(PORT, () => {
-  console.log("Graphql server now up at port ", PORT)
-});
